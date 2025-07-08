453cc13bbc04d5f608a60fe7354671424db6f3b7
Horimatsu Takuya <new_woory@hotmail.com>
2025-07-08 20:26:01 +0900

    fix: init

---
+++ scripts/run-lighthouse.js
@@ -0,0 +1,24 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+const fs = require('fs');
+const path = require('path');
+
+const url = 'https://example.com';
+
+(async () => {
+  const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
+  const options = { port: chrome.port, output: ['html', 'json'] };
+  const runnerResult = await lighthouse(url, options);
+
+  // 保存先ディレクトリを作成
+  const outputDir = path.resolve(__dirname, '../report');
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
+
+  // HTMLとJSONで保存
+  fs.writeFileSync(path.join(outputDir, 'report.html'), runnerResult.report[0], 'utf-8');
+  fs.writeFileSync(path.join(outputDir, 'report.json'), runnerResult.report[1], 'utf-8');
+
+  await chrome.kill();
+})();
